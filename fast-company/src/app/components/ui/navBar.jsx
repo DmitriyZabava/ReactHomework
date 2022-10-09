05f835b670d6fa058123a668916a204303fd8261
DmitriyZabava <102691652+DmitriyZabava@users.noreply.github.com>
2022-10-09 12:07:44 +0300

    some correct

--- fast-company/src/app/components/ui/navBar.jsx
+++ fast-company/src/app/components/ui/navBar.jsx
@@ -8,34 +8,43 @@ const NavBar = () => {
     const handleAuthExit = () => setAuth(false);
 
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <Link className="nav-link" to="/">
-                    <i className="px-1 bi bi-house" />
-                    Главная
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to={isAuth ? "/users" : "/login"}>
-                    Пользователи
-                </Link>
-            </li>
-            {isAuth ? (
-                <li className="nav-item" onClick={handleAuthExit}>
-                    <Link className="nav-link" to="/login">
-                        <i className="px-1 bi bi-box-arrow-left" />
-                        Выйти
-                    </Link>
-                </li>
-            ) : (
-                <li className="nav-item">
-                    <Link className="nav-link" to="/login">
-                        <i className="px-1 bi bi-box-arrow-in-right" />
-                        Вход/Регистрация
-                    </Link>
-                </li>
-            )}
-        </ul>
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid nav-tabs">
+                <ul className="nav ">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">
+                            <i className="px-1 bi bi-house" />
+                            Главная
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            className="nav-link"
+                            to={isAuth ? "/users" : "/login"}
+                        >
+                            Пользователи
+                        </Link>
+                    </li>
+                </ul>
+                <ul className="nav ">
+                    {isAuth ? (
+                        <li className="nav-item" onClick={handleAuthExit}>
+                            <Link className="nav-link" to="/login">
+                                <i className="px-1 bi bi-box-arrow-left" />
+                                Выйти
+                            </Link>
+                        </li>
+                    ) : (
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">
+                                <i className="px-1 bi bi-box-arrow-in-right" />
+                                Вход/Регистрация
+                            </Link>
+                        </li>
+                    )}
+                </ul>{" "}
+            </div>
+        </nav>
     );
 };
 
